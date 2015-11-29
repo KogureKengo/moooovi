cc202e93e4276b60c58717f5b6b9ba6a2b74de51
KogureKengo <a.kagi.ten@gmail.com>
2015-11-29 14:36:30 +0900

    productテーブルを書く

--- db/migrate/20151129052031_create_products.rb
+++ db/migrate/20151129052031_create_products.rb
@@ -3,6 +3,9 @@ class CreateProducts < ActiveRecord::Migration
     create_table :products do |t|
       t.string  :title
       t.text    :image_url
+      t.string  :director
+      t.text    :detail
+      t.string  :open_date
       t.timestamps
     end
   end
